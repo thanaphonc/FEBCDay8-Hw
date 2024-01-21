012f07decfca1f38d6960b946fadf49d9a0927c3
___ooaatt <64412487+thanaphonc@users.noreply.github.com>
2024-01-21 11:33:22 +0700

    Add file

---
+++ app.js
@@ -0,0 +1,11 @@
+const ToDoLists = document.getElementById("todo-lists");
+
+function AddToDoItems() {
+  let input = document.getElementById("input-todo");
+  //console.log(item);
+  const li = document.createElement("li");
+  const item = `<div>${input.value}</div><button type="button" class="btn-delete" onclick="this.parentNode.style.display='none';">ลบ</button>`;
+  li.innerHTML = item;
+  ToDoLists.appendChild(li);
+  input.value = "";
+}
