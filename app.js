db6bbc5078cc1b8f3f14fb26f98166801f8116c6
___ooaatt <64412487+thanaphonc@users.noreply.github.com>
2024-01-21 11:36:14 +0700

    Update app.js
    remove console.log

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ const ToDoLists = document.getElementById("todo-lists");
 
 function AddToDoItems() {
   let input = document.getElementById("input-todo");
-  //console.log(item);
   const li = document.createElement("li");
   const item = `<div>${input.value}</div><button type="button" class="btn-delete" onclick="this.parentNode.style.display='none';">ลบ</button>`;
   li.innerHTML = item;
